240ee63ea8faf901365265555d0aca60bffca8f9
Aleksey Kravtsun <kravtsun.aleksey@gmail.com>
2023-03-30 21:20:43 +0300

    Initial commit

---
+++ users/models.py
@@ -0,0 +1,42 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class Counterparty(models.Model):
+    """
+    Контрагенты.
+    """
+    name = models.CharField('Название',
+                            max_length=150,
+                            unique=True)
+
+    class Meta:
+        verbose_name = 'Контрагент'
+        verbose_name_plural = 'Контрагенты'
+        ordering = ['name']
+
+
+class User(AbstractUser):
+    """
+    Пользователи.
+    """
+
+    REQUIRED_FIELDS = ['email', 'first_name', 'last_name']
+
+    email = models.EmailField('Email',
+                              unique=True)
+    first_name = models.CharField('Имя',
+                                  max_length=150)
+    last_name = models.CharField('Фамилия',
+                                 max_length=150)
+    counterparty = models.ForeignKey(Counterparty,
+                                     blank=True,
+                                     null=True,
+                                     on_delete=models.CASCADE,
+                                     related_name='employees',
+                                     verbose_name='Контрагент')
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+        ordering = ['first_name']
