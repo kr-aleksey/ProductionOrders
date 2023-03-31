4842d5e1c9c4576185e24cc07f74145cb6e69d1a
kr-aleksey <kravtsun.aleksey@gmail.com>
2023-03-31 09:37:17 +0300

    Users admin

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.contenttypes.admin import GenericTabularInline
 
-# Register your models here.
+from .models import Counterparty, User
+
+
+class UsersInline(GenericTabularInline):
+    model = User
+    fields = ('username',)
+
+
+@admin.register(Counterparty)
+class CounterpartyAdmin(admin.ModelAdmin):
+    fields = ('name',)
+    inlines = (UsersInline,)
+
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    search_fields = ('first_name',
+                     'last_name',
+                     'username',
+                     'counterparty__name')
+    list_display = ('username',
+                    'first_name',
+                    'last_name',
+                    'is_active',
+                    'is_staff')
